bffef0f93d858e9c7c6894244ce331d0117c69bf
a-poor <a-poor@users.noreply.github.com>
2021-12-17 20:11:06 -0800

    Running lex test

--- cypher.go
+++ cypher.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+
+	"github.com/a-poor/cypher/parser"
+)
+
 func main() {
+	q := `MATCH (:Person {name: "Alice"})-[:KNOWS]->(:Person {name: "Bob"}) RETURN count(*)`
 
+	// Setup the input
+	is := antlr.NewInputStream(q)
+
+	// Create the Lexer
+	lexer := parser.NewCypherLexer(is)
+
+	// Read all tokens
+	for {
+		t := lexer.NextToken()
+		if t.GetTokenType() == antlr.TokenEOF {
+			break
+		}
+		fmt.Printf("%s (%q)\n",
+			lexer.SymbolicNames[t.GetTokenType()], t.GetText())
+	}
 }
-
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/a-poor/cypher
 
 go 1.17
+
+require github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20211213210530-5d6a78255383
