928d7e16114cf5aa8293d08d1753c0a63b7a1921
a-poor <45295232+a-poor@users.noreply.github.com>
2022-01-21 07:23:15 -0800

    Testing yacc approach

--- cypher.go
+++ cypher.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"log"
+	"os"
+
+	"golang.org/x/exp/ebnf"
+)
+
+const ebnfPath = "cypher.ebnf"
 
 func main() {
+	//query := `MATCH (n {name: "John"})-[:FRIEND]-(friend) WITH n, count(friend) AS friendsCount WHERE friendsCount > 3 RETURN n, friendsCount`
+	//fmt.Printf("Query: %s\n", query)
 
-	query := `MATCH (n {name: "John"})-[:FRIEND]-(friend) WITH n, count(friend) AS friendsCount WHERE friendsCount > 3 RETURN n, friendsCount`
-	fmt.Printf("Query: %s\n", query)
+	in, err := os.ReadFile(ebnfPath)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	gram, err := ebnf.Parse(ebnfPath, bytes.NewReader(in))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Grammar length: %d\n", len(gram))
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/a-poor/cypher
 
 go 1.17
+
+require golang.org/x/exp v0.0.0-20211210185655-e05463a05a18
+
+require golang.org/x/tools v0.1.8 // indirect
